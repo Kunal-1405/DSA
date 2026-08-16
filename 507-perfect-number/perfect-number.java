2405be66f60de79e3486f372a9990d13a96553c7
KunalV <kunalvyas118@gmail.com>
2026-08-16 14:41:41 +0530

    Time: 2 ms (82.95%) | Memory: 42.2 MB (45.25%) - LeetSync

---
+++ 507-perfect-number/perfect-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if (num <= 1) return false;
+        int sum = 1; // 1 is always a proper divisor for num > 1
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                sum += i;
+                if (i * i != num) {
+                    sum += num / i; // add the paired divisor
+                }
+            }
+        }
+        return sum == num;
+    }
+}
