28a126d906d1aad3ef5351c94a0b694104e19ecb
KunalV <kunalvyas118@gmail.com>
2026-08-31 21:02:16 +0530

    Time: 41 ms (93.96%) | Memory: 59.9 MB (72.93%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.java
+++ 493-reverse-pairs/reverse-pairs.java
@@ -1,53 +1,50 @@
 class Solution {
     public int reversePairs(int[] nums) {
-         return mergeSort(nums, 0, nums.length - 1);
+        int[] temp = new int[nums.length];
+        return mergeSort(nums, temp, 0, nums.length - 1);
     }
-    private int mergeSort(int[] nums, int low, int high) {
+
+    private int mergeSort(int[] nums, int[] temp, int low, int high) {
         if (low >= high) return 0;
 
-        int mid = (low + high) / 2;
+        int mid = low + (high - low) / 2;
         int cnt = 0;
 
-        cnt += mergeSort(nums, low, mid);
-        cnt += mergeSort(nums, mid + 1, high);
+        cnt += mergeSort(nums, temp, low, mid);
+        cnt += mergeSort(nums, temp, mid + 1, high);
         cnt += countPairs(nums, low, mid, high);
-        merge(nums, low, mid, high);
+        merge(nums, temp, low, mid, high);
 
         return cnt;
     }
 
-    // Count reverse pairs
     private int countPairs(int[] nums, int low, int mid, int high) {
         int right = mid + 1, cnt = 0;
-
         for (int i = low; i <= mid; i++) {
             while (right <= high && (long) nums[i] > 2L * nums[right]) {
                 right++;
             }
             cnt += (right - (mid + 1));
         }
-
         return cnt;
     }
 
-    // Merge two sorted halves
-    private void merge(int[] nums, int low, int mid, int high) {
-        List<Integer> temp = new ArrayList<>();
-        int left = low, right = mid + 1;
+    private void merge(int[] nums, int[] temp, int low, int mid, int high) {
+        int left = low, right = mid + 1, k = low;
 
         while (left <= mid && right <= high) {
             if (nums[left] <= nums[right]) {
-                temp.add(nums[left++]);
+                temp[k++] = nums[left++];
             } else {
-                temp.add(nums[right++]);
+                temp[k++] = nums[right++];
             }
         }
 
-        while (left <= mid) temp.add(nums[left++]);
-        while (right <= high) temp.add(nums[right++]);
+        while (left <= mid) temp[k++] = nums[left++];
+        while (right <= high) temp[k++] = nums[right++];
 
         for (int i = low; i <= high; i++) {
-            nums[i] = temp.get(i - low);
+            nums[i] = temp[i];
         }
     }
 }
